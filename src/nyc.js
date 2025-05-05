d08901a3d313b7e7d71da0d45019a26382386919
Rivoangel <makaringerivoningo02@gmail.com>
2025-05-05 02:18:56 +0200

    api

--- src/nyc.js
+++ src/nyc.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "c4904cc93otb1dacc041bcba0f2ab309";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
