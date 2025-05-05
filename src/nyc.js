a4558c2a217146e51c1e0fe5eb6311ac405d0e5f
Rivoangel <makaringerivoningo02@gmail.com>
2025-05-05 03:49:57 +0200

    time and date

--- src/nyc.js
+++ src/nyc.js
@@ -3,15 +3,18 @@ let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
   let descriptionElement= document.querySelector("#description");
-  let humidityElement= document.querySelector("#humidity")
-  let windSpeedElement=document.querySelector("#wind-speed")
+  let humidityElement= document.querySelector("#humidity");
+  let windSpeedElement=document.querySelector("#wind-speed");
+  let currentDateElement=document.querySelector("#current-date");
+  let date= new Date(response.data.time * 1000)
   
-  console.log(response.data.)
+  console.log(response.data)
   
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
-  windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`
+  windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
+  currentDateELement.innerHTML= formatDate(date)//`${date.getDay()} ${date.getHours()}:${date.getMinutes()}`
   temperatureElement.innerHTML = temperature;
 }
 
